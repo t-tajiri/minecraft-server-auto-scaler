fdf2052d70fb6cb216fd46bf474f3f288d842c14
t-tajiri <swallow.ava@gmail.com>
2025-04-12 22:19:17 +0900

    move src folder

--- discord_notifier/build.sh
+++ discord_notifier/build.sh
@@ -4,4 +4,4 @@ rm -f deployment_packakages.zip
 cd .venv/lib/python3.13/site-packages
 zip -r ../../../../deployment_packages.zip .
 cd ../../../../
-zip deployment_packages.zip lambda_function.py
+zip deployment_packages.zip src/lambda_function.py
