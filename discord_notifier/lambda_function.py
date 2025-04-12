88fc97dc67cb0d258f2a0dc2547c5508b01f3909
t-tajiri <swallow.ava@gmail.com>
2025-04-12 16:59:07 +0900

    format code

--- discord_notifier/lambda_function.py
+++ discord_notifier/lambda_function.py
@@ -1,8 +1,10 @@
 import json
-from discord_interactions import verify_key, InteractionType, InteractionResponseType
 import os
 
+from discord_interactions import InteractionResponseType, InteractionType, verify_key
+
 discord_public_key = os.environ.get("DISCORD_PUBLIC_KEY", "")
+
 
 def lambda_handler(event, context):
     request_body = json.loads(event.get("body", "{}"))
@@ -12,17 +14,20 @@ def lambda_handler(event, context):
     signature = headers.get("x-signature-ed25519")
     timestamp = headers.get("x-signature-timestamp")
     raw_body = event.get("body", "{}").encode()
-    if signature is None or timestamp is None or \
-        not verify_key(raw_body, signature, timestamp, discord_public_key): # Authorization
-        return {
-            'statusCode': 401,
-            'body': "Bad request signature"
-        }
+    if (
+        signature is None
+        or timestamp is None
+        or not verify_key(raw_body, signature, timestamp, discord_public_key)
+    ):  # Authorization
+        return {"statusCode": 401, "body": "Bad request signature"}
 
     # Handle request
     interaction_type = request_body.get("type")
 
-    if interaction_type in [InteractionType.APPLICATION_COMMAND, InteractionType.MESSAGE_COMPONENT]:
+    if interaction_type in [
+        InteractionType.APPLICATION_COMMAND,
+        InteractionType.MESSAGE_COMPONENT,
+    ]:
         data = request_body.get("data", {})
         command_name = data.get("name")
 
@@ -35,14 +40,9 @@ def lambda_handler(event, context):
 
         response_data = {
             "type": InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-            "data": {
-                "content": response_text
-            }
+            "data": {"content": response_text},
         }
     else:
-        response_data = { "type": InteractionResponseType.PONG }
+        response_data = {"type": InteractionResponseType.PONG}
 
-    return {
-        'statusCode': 200,
-        'body': json.dumps(response_data)
-    }
+    return {"statusCode": 200, "body": json.dumps(response_data)}
